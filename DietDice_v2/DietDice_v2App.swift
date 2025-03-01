941dd515fd3b07cf43d269c7af8acc5abcced0fa
wangdairou <wang.dair@northeastern.edu>
2025-03-01 00:21:25 -0800

    Initial Commit

---
+++ DietDice_v2/DietDice_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  DietDice_v2App.swift
+//  DietDice_v2
+//
+//  Created by Darol on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DietDice_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
